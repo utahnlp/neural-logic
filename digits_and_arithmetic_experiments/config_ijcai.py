a743bca901d01e231e6863225be39478234d4865
MattiaGrespan <mattiamedina@gmail.com>
2021-07-18 15:21:21 -0600

    config changes

--- digits_and_arithmetic_experiments/config_ijcai.py
+++ digits_and_arithmetic_experiments/config_ijcai.py
@@ -3,6 +3,8 @@ HOME_DIR=""
 # Data Parameters
 #DATA_DIR="data/" #ASHIM
 DATA_DIR ="/uusoc/scratch/bluefish/mattiamg/ijcai2021/data" #MATTIA
+#DATA_DIR = "data/"
+
 SINGLE_MNIST_FILE="single_mnist_size_{}_seed_{}.pt"
 OPERATOR_MNIST_FILE="option_{}_mnist_size_{}_operator_size_{}_{}_seed_{}.pt"
 # For experiments sept
@@ -20,7 +22,10 @@ DISTRIBUTIVITY_MNIST_TEST="distributivity_test_size_{}_seed_{}.pt"
 #Model related
 #MODEL_DIR="/uusoc/scratch/bluefish/mattiamg/demo_data/models"
 MODEL_DIR = "/uusoc/scratch/bluefish/mattiamg/ijcai2021/models"
+#MODEL_DIR = "models/"
+
 JOINT_MODEL_DIR = "/uusoc/scratch/bluefish/mattiamg/ijcai2021/models/joint_models"
+#JOINT_MODEL_DIR = "models/joint_models"
 JOINT_DIGIT_MODEL_NAME = "digit_models/joint_digit_option_{}_digit_size_{}_pair_size_{}_lr_{}_optim_{}_tnorm_{}_weight_{}_seed_{}_epochs_{}.pt"
 JOINT_SUM_MODEL_NAME = "sum_models/joint_sum_option_{}_digit_size_{}_pair_size_{}_lr_{}_optim_{}_tnorm_{}_weight_{}_seed_{}_epochs_{}.pt"
 JOINT_PROD_MODEL_NAME = "prod_models/joint_prod_option_{}_digit_size_{}_pair_size_{}_lr_{}_optim_{}_tnorm_{}_weight_{}_seed_{}_epochs_{}.pt"
